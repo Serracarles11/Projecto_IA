94a24a457bda535b54018ea3af318026df551000
Serracarles11 <Carlesserra685@gmail.com>
2025-03-25 10:42:34 +0100

    header acabado

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".todo_header",{
-    opacity: '0',
-    scrollTrigger:{
-        trigger:".abajo",
-        start:"top center",
-        end:"bottom center",
-        scrub:true
+gsap.to(".todo_header", {
+    opacity: 0.7, // No lo hace desaparecer del todo, lo hace más elegante
+    backgroundColor: "rgba(128, 128, 128, 0.8)", // Un azul translúcido bonito
+    duration: 2, // Hace la transición más lenta y fluida
+    ease: "power2.out", // Suaviza la animación
+    scrollTrigger: {
+        trigger: ".abajo",
+        start: "top+=100px center",
+        end: "bottom center",
+        scrub: 2, // Hace que el cambio sea progresivo
     }
-})
+});
+
 gsap.to(".background_header",{
-    backgroundColor: 'grey', // Corrección aquí
+    visibility: 'visible',
+    opacity:'1',
+    y: 80, 
+
     scrollTrigger:{
+        markers:true,
         trigger:".abajo",
-        start:"top center",
-        end:"bottom center",
+        start:"top+=315px center",
+        end:"bottom-=30px center",
         scrub:true
     }
 })
+ScrollSmoother.create({
+    smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
+    effects: true, // looks for data-speed and data-lag attributes on elements
+    smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  });
