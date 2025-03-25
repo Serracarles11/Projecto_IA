9b3e7d40583a2b41f572da253165bdfc4edb378c
Serracarles11 <Carlesserra685@gmail.com>
2025-03-25 10:17:48 +0100

    los cambios del header

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".todo_header",{
+    opacity: '0',
+    scrollTrigger:{
+        trigger:".abajo",
+        start:"top center",
+        end:"bottom center",
+        scrub:true
+    }
+})
+gsap.to(".background_header",{
+    backgroundColor: 'grey', // Corrección aquí
+    scrollTrigger:{
+        trigger:".abajo",
+        start:"top center",
+        end:"bottom center",
+        scrub:true
+    }
+})
